0ce6e5615a34a9c6b4a26550110f80160a073aec
YanyanDai <47199662+YanyanDai@users.noreply.github.com>
2023-01-31 22:44:31 +0900

    Update listener.py

--- scripts/listener.py
+++ scripts/listener.py
@@ -2,8 +2,8 @@
 import rospy
 from std_msgs.msg import String
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+
+
     
 def listener():
 
@@ -12,12 +12,12 @@ def listener():
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
-
-    rospy.Subscriber("chatter", String, callback)
+    
+    
+    
 
     # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+
 
 if __name__ == '__main__':
     listener()
