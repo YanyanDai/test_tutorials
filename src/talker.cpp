c055a8a6dc54dbfaf7b2b297d435e63bc82f150e
YanyanDai <47199662+YanyanDai@users.noreply.github.com>
2023-02-01 22:25:27 +0900

    Update talker.cpp

--- src/talker.cpp
+++ src/talker.cpp
@@ -5,10 +5,12 @@
 
 int main(int argc, char **argv)
 {
-  
-  
-  
-  
+  ros::init(argc, argv, "talker");
+
+  ros::NodeHandle n;
+
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+
   ros::Rate loop_rate(10);
 
   int count = 0;
@@ -22,13 +24,14 @@ int main(int argc, char **argv)
 
     ROS_INFO("%s", msg.data.c_str());
 
-    
-    
+    chatter_pub.publish(msg);
 
+    ros::spinOnce();
+
+    loop_rate.sleep();
     ++count;
   }
 
 
   return 0;
 }
-
